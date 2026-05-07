ba1831e437943874a3c41dec7af8e6818e480e85
Muhammad Faisal <199878514+MFaisal-0@users.noreply.github.com>
2026-05-07 10:05:54 +0500

    Add file

---
+++ practice-1/script.js
@@ -0,0 +1,35 @@
+console.log("Faisal");
+
+// let cont = document.body.childNodes[1];
+
+// cont
+
+// cont.firstChild
+
+// cont.lastChild
+
+// cont.firstElementChild
+
+// cont.childElementCount
+
+// cont.lastElementChild.style.color = "green";
+
+// cont.lastElementChild.style.backgroundColor = "red";
+
+// document.body.firstElementChild.
+// document.body.firstElementChild.childNodes//Text nodes, Element nodes
+// document.body.firstElementChild.children//Element nodes
+
+// document.body.firstElementChild.children[0]
+// document.body.firstElementChild.children[1]
+// document.body.firstElementChild.children[2]
+// document.body.firstElementChild.children[3]
+// document.body.firstElementChild.children[4]
+
+// document.body.firstElementChild.children[3].nextElementSibling //Next Element nodes
+// document.body.firstElementChild.children[3].nextSibling// next Text nodes
+
+// document.body.firstElementChild.children[3].previousElementSibling//Previous Element nodes
+// document.body.firstElementChild.children[3].previousSibling//Previous Text nodes
+
+// document.body.firstElementChild.children[0].prarentElement
