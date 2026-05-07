a91c9410aa064a045fcad2a1ea98cfdcb598eea0
Muhammad Faisal <199878514+MFaisal-0@users.noreply.github.com>
2026-05-07 10:06:15 +0500

    Add file

---
+++ practice-2/script.js
@@ -0,0 +1,25 @@
+console.log("Faisal");
+
+/*          getElementsByClass
+
+ let boxes = document.getElementsByClassName("box");
+  console.log(boxes);
+
+ boxes[2].style.backgroundColor = "white"; 
+ boxes[2].style.color = "black";*/ 
+
+ /*         getElementById
+
+ document.getElementById("white").style.backgroundColor = "white";
+ document.getElementById("white").style.color = "black";*/
+
+ /*         querySelector
+
+ document.querySelector(".box").style.backgroundColor = "orange";
+document.querySelectorAll(".box").forEach(e => {
+    e.style.backgroundColor = "orange";
+})*/
+
+ /*           getElementsByTag
+
+document.getElementsByTagName("div");*/
