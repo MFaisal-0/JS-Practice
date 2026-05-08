a4367e2db69c8e1325ce1f8ee5248481f27e9641
Muhammad Faisal <199878514+MFaisal-0@users.noreply.github.com>
2026-05-08 23:05:22 +0500

    Add file

---
+++ Error-Handling/script.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 7;
+
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(b + a);
+try {
+    console.log(a - f);
+} catch(err){
+    console.log(err)
+}
+console.log(b - a);
+console.log(a * b);
+console.log(b * a);
